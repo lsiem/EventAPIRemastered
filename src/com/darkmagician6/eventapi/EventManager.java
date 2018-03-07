46c5d618bae2e02b799cdd280e82ccc324a224d8
IntenceYT <lassetv2001@gmail.com>
2018-03-07 13:20:26 +0100

    Remove final modifier in call method
    final modifier not needed here.

--- src/com/darkmagician6/eventapi/EventManager.java
+++ src/com/darkmagician6/eventapi/EventManager.java
@@ -239,7 +239,7 @@ public final class EventManager {
      *
      * @return Event in the state after dispatching it.
      */
-    public static final Event call(final Event event) {
+    public static Event call(final Event event) {
         List<MethodData> dataList = REGISTRY_MAP.get(event.getClass());
 
         if (dataList != null) {
